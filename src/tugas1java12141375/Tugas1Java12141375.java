6582e539978ec767ada7aa72afaf1b3ce1add65d
Prisyaji <aji18suroso@gmail.com>
2016-11-04 15:27:36 +0700

    12141375 Pada tampilan yang dibuat bisa dilihat adanya source code JOptionPane yang merupakan salah satu class yang terdapat pada javax.swing, fungsi itu sendiri adalah untuk menampilkan jendela dalam tampilan yang akan memunculkan informasi. kemudian untuk public static void main sendiri memiliki kegunaan menjadi sebuah fungsi main yang dimana public sendiri merupakan metode yang dapat dipanggil secara fleksibel.

---
+++ src/tugas1java12141375/Tugas1Java12141375.java
@@ -0,0 +1,10 @@
+package tugas1java12141375;
+
+import javax.swing.JOptionPane;
+
+public class Tugas1Java12141375 {
+    
+    public static void main(String[] args) {
+    
+    JOptionPane.showMessageDialog(null, "Tugas 1 Pemograman Java\n Nama : Prisyliawan Aji Suroso\n Nim : 12141375");
+     
