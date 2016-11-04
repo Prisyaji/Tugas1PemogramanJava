91247e156d9c49de58b2877033521f00666cc504
Prisyaji <aji18suroso@gmail.com>
2016-11-04 15:33:54 +0700

    12141375 pada tampilan berikut terdapat variabel integer yang merupakan type data untuk menjalankan program yang akan dikerjakan, tampilan yang diharapkan adalah bentuk segitiga siku kiri atas.

--- src/tugas1java12141375/Tugas1Java12141375.java
+++ src/tugas1java12141375/Tugas1Java12141375.java
@@ -7,4 +7,16 @@ public class Tugas1Java12141375 {
     public static void main(String[] args) {
     
     JOptionPane.showMessageDialog(null, "Tugas 1 Pemograman Java\n Nama : Prisyliawan Aji Suroso\n Nim : 12141375");
-     
+    // inisialisasi variabel
+     int jumlah;
+        
+        jumlah = Integer.valueOf(JOptionPane.showInputDialog("Jumlah Karakter Segitiga : "));
+                
+        // Bentuk Segitiga Siku Kiri Atas 
+        System.out.println("Bentuk Segitiga siku Kiri Atas");
+        for(int a=0;a<jumlah;a++) {
+            for(int b=a;b<jumlah;b++) {
+                System.out.print("*");
+            }
+        System.out.println();
+        }
